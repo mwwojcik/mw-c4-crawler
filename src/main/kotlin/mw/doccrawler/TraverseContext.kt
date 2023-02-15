8476d989b823f8993310d580b3585257b4dcdabd
Mariusz Wojcik <mariusz.wojcik@allegropay.pl>
2023-02-15 14:05:54 +0100

    supertype relations

--- src/main/kotlin/mw/doccrawler/TraverseContext.kt
+++ src/main/kotlin/mw/doccrawler/TraverseContext.kt
@@ -1,101 +1,69 @@
 package mw.doccrawler
 
-import org.jetbrains.kotlin.asJava.toLightElements
 import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtParameter
-import org.jetbrains.kotlin.psi.KtParameterList
-import org.jetbrains.kotlin.psi.KtPrimaryConstructor
-import org.jetbrains.kotlin.psi.KtProperty
-import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
-import org.jetbrains.kotlin.psi.KtTypeArgumentList
-import org.jetbrains.kotlin.psi.KtTypeProjection
-import org.jetbrains.kotlin.psi.KtTypeReference
 import org.jetbrains.kotlin.psi.KtUserType
-import org.jetbrains.kotlin.psi.KtValueArgumentList
-import org.jetbrains.kotlin.resolve.BindingContextUtils
-import org.jetbrains.kotlin.resolve.resolveQualifierAsReceiverInExpression
+import org.jetbrains.kotlin.psi.psiUtil.getSuperNames
 
 
 class TraverseContext(
-    val ktElements: List<KtClass> = emptyList(),
-    val exclusions: List<String> = emptyList()
+    ktElements: List<KtClass> = emptyList(),
+    exclusions: List<String> = emptyList()
 ) {
-    var elements: Map<String, KtClass> = emptyMap()
-    var roots: List<KtClass> = emptyList()
+    var elements: List<KtClassItem> = emptyList()
+    var roots: List<KtClassItem> = emptyList()
 
     init {
-        elements = ktElements.filter { psi -> exclusions.none { psi.fqName?.asString()?.contains(it) ?: true } }
-            .map { it.fqName!!.asString()!! to it }
-            .toMap()
-
-        roots=elements.filterKeys { it.contains("Endpoint") }.values.toList()
+        val implementations: MutableMap<String, MutableSet<KtClassItem>> = mutableMapOf()
+        elements = ktElements.map { KtClassItem(it.name!!, it.fqName!!.asString(), it) }.toList()
+        elements.forEach { collectSupertypeRelations(it, implementations) }
+        elements = elements.map {
+            if (it.isInterface()) {
+                implementations[it.shortName]?.let { it1 -> it.copy(knownImplementations = it1) } ?: it
+            } else {
+                it
+            }
+        }.toList()
+        roots = elements.filter { it.shortName.contains("Endpoint") }.toList()
     }
 
-    fun traverse(){
-
-        roots.get(0).accept(MyKtVisitor())
-
-
-       roots.forEach { visit(it) }
+    fun traverse() {
+        roots.forEach { visit(it) }
     }
 
-    private fun visit(it: KtClass) {
-
-        println("visited=>${it.name}")
-        //it.getChildrenOfType<KtClass>().asList().forEach { visit(it) }
-
-        it.primaryConstructor?.typeParameters?.forEach{
-            child->
-            println(child)
+    private fun visit(it: KtClassItem) {
+        println("visited=>${it.ktClass.name}")
+        it.ktClass.primaryConstructor?.valueParameterList?.parameters?.forEach {
+            if (it.typeReference?.typeElement is KtUserType) {
+                val typeShortName = (it.typeReference?.typeElement as KtUserType).referencedName
+                println(typeShortName)
+                resolveTypeByShortName(shortName = typeShortName!!)
+            }
         }
+    }
+
+    private fun collectSupertypeRelations(
+        ktItem: KtClassItem,
+        implementations: MutableMap<String, MutableSet<KtClassItem>>
+    ) {
+        if (ktItem.ktClass.getSuperNames().isNullOrEmpty())
+            return
+        ktItem.ktClass.getSuperNames().forEach {
+            if (resolveTypeByShortName(it)?.ktClass?.isInterface() == true)
+                implementations.computeIfAbsent(it) { mutableSetOf() }.add(ktItem)
+        }
+    }
+
+    fun resolveTypeByShortName(shortName: String): KtClassItem? {
+        return elements.firstOrNull { it.shortName == shortName }
     }
 }
 
-class MyKtVisitor:KtTreeVisitorVoid(){
 
-    override fun visitPrimaryConstructor(constructor: KtPrimaryConstructor) {
-
-        constructor.valueParameterList?.parameters?.forEach {
-            if (it.typeReference?.typeElement is KtUserType) {
-                val myRef=it.typeReference
-
-                val typeShortName = (it.typeReference?.typeElement as KtUserType).referencedName
-                println(typeShortName)
-            }
-        }
-        super.visitPrimaryConstructor(constructor)
-    }
-
-    override fun visitParameterList(list: KtParameterList) {
-        super.visitParameterList(list)
-    }
-
-    override fun visitTypeProjection(typeProjection: KtTypeProjection) {
-        super.visitTypeProjection(typeProjection)
-    }
-
-    override fun visitValueArgumentList(list: KtValueArgumentList) {
-        super.visitValueArgumentList(list)
-    }
-
-    override fun visitTypeArgumentList(typeArgumentList: KtTypeArgumentList) {
-        super.visitTypeArgumentList(typeArgumentList)
-    }
-
-    override fun visitParameter(parameter: KtParameter) {
-        super.visitParameter(parameter)
-    }
-
-    override fun visitUserType(type: KtUserType) {
-        super.visitUserType(type)
-    }
-
-    override fun visitProperty(property: KtProperty) {
-        super.visitProperty(property)
-    }
-
-    override fun visitTypeReference(typeReference: KtTypeReference) {
-        super.visitTypeReference(typeReference)
-        typeReference.toLightElements()
-    }
+data class KtClassItem(
+    val shortName: String,
+    val fullName: String,
+    val ktClass: KtClass,
+    var knownImplementations: Set<KtClassItem> = emptySet()
+) {
+    fun isInterface() = ktClass.isInterface()
 }
